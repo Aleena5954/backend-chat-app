818bf6863b460cf76d471201eb24b78af6bd3cad
Aleena <aleenaafaq7@gmail.com>
2025-01-25 20:11:44 +0530

    commit1

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://neon-pie-fbc40c.netlify.app/",
+    origin: "https://neon-pie-fbc40c.netlify.app",
     credentials: true,
   })
 );
