5b543e8790643a7661ebb2b70861f617f03eeed6
Aleena <aleenaafaq7@gmail.com>
2025-01-25 20:35:07 +0530

    commit

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,3 +1,41 @@
+// import { Server } from "socket.io";
+// import http from "http";
+// import express from "express";
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["https://neon-pie-fbc40c.netlify.app"],
+//   },
+// });
+
+// export function getReceiverSocketId(userId) {
+//   return userSocketMap[userId];
+// }
+
+// // used to store online users
+// const userSocketMap = {}; // {userId: socketId}
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected", socket.id);
+
+//   const userId = socket.handshake.query.userId;
+//   if (userId) userSocketMap[userId] = socket.id;
+
+//   // io.emit() is used to send events to all the connected clients
+//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected", socket.id);
+//     delete userSocketMap[userId];
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//   });
+// });
+
+// export { io, app, server };
+
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -11,26 +49,59 @@ const io = new Server(server, {
   },
 });
 
+// Used to store online users
+const userSocketMap = {}; // { userId: socketId }
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
+  // Save the user ID and socket ID
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    // Notify all users that a user has come online
+    io.emit("userOnlineStatus", {
+      userId,
+      status: "online",
+      onlineUsers: Object.keys(userSocketMap),
+    });
+  }
 
+  // Emit the list of online users to the newly connected user
+  socket.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Listen for a new message event
+  socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+    console.log(`Message from ${senderId} to ${receiverId}:`, message);
+
+    // Send the message to the receiver if they're online
+    const receiverSocketId = userSocketMap[receiverId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", { senderId, message });
+    }
+
+    // Optionally, notify the sender that the message was sent successfully
+    socket.emit("messageSent", { success: true });
+  });
+
+  // Handle user disconnect
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    if (userId) {
+      delete userSocketMap[userId];
+
+      // Notify all users that a user has gone offline
+      io.emit("userOnlineStatus", {
+        userId,
+        status: "offline",
+        onlineUsers: Object.keys(userSocketMap),
+      });
+    }
   });
 });
 
